5900391ae064d941582297238d43e4bea8f6d9a7
Fernando <fbellas@udc.es>
2021-03-26 18:05:40 +0100

    Actualización a create-react-app 4.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import * as serviceWorker from './serviceWorker';
 import App from './App';
 import './styles.css';
 
@@ -11,5 +10,3 @@ ReactDOM.render(
         <App/>
     </React.StrictMode>,
     document.getElementById('root'));
-
-serviceWorker.register();
